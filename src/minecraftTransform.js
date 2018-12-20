6b1892b14e08aad9f2889680a29859fe7fa0dedd
Sceat <11330271+Sceat@users.noreply.github.com>
2018-12-20 10:44:47 +0200

    improve the computeKnown transform stream

--- src/minecraftTransform.js
+++ src/minecraftTransform.js
@@ -1,62 +1,57 @@
 import { Transform } from 'stream'
-import MapColor from './mapColors'
+import { fromImage } from './mapColors'
+import cwise from 'cwise'
+import ndarray from 'ndarray'
 
 export class ComputeColors extends Transform {
-	// buffer to id array
-	_transform(chunk, _, next) {
-		MapColor.fromImage(chunk)
-			.then(this.push)
-			.then(next)
-	}
-}
-
-export class ComputeKnown extends Transform {
-	constructor(width, height) {
+	constructor() {
 		super({ readableObjectMode: true })
-		Object.assign(this, { width, height })
-		this.last = new Uint8Array(width * height)
 	}
 
-	_transform(chunk, _, next) {
-		let columns = {}
-		let rows = {}
-		for (let x = 0; x < this.width; x++)
-			for (let y = 0; y < this.height; y++) {
-				const index = x + this.width * y
-				if (chunk[index] !== this.last[index]) {
-					if (!columns.start) columns.start = x
-					if (!rows.start) rows.start = y
-					if (!rows.end || y > rows.end) rows.end = y
-					columns.end = x
-				}
+	// buffer to id ndArray
+	async _transform(chunk, _, next) {
+		const { width, height, datas } = await fromImage(chunk.toString('utf8')) // TODO find which imput i should listen for
+		this.push(ndarray(datas, [width, height]))
+		next()
+	}
+}
+
+// ndarray to ndarray
+export class ComputeKnown extends Transform {
+	constructor() {
+		super({ objectMode: true })
+	}
+
+	compute = cwise({
+		args: ['index', 'array', 'array'],
+		pre: function() {
+			this.start = 0
+			this.end = 0
+		},
+		body: function(index, last, curr) {
+			// doesn't transpile :(
+			const x = index[0]
+			const y = index[1]
+			if (last !== curr) {
+				if (!this.start) this.start = { x: x, y: y }
+				this.end = { x: x + 1, y: y + 1 }
 			}
-		if (columns.start) {
-			const column = columns.end - columns.start || 1 // if only one column updated then we take 1
-			const row = rows.end - rows.start || 1
-			const x = column.start
-			const y = rows.start
-			this.push({ column, row, x, y, chunk })
+		},
+		post: function() {
+			if (this.start) return { start: this.start, end: this.end }
+		},
+	})
+
+	_transform(_chunk, _, next) {
+		if (!this.last) this.push({ x: 0, y: 0, chunk: _chunk })
+		else {
+			const { start, end } = this.compute(this.last, _chunk) || {}
+			const { x: startX, y: startY } = start || {}
+			const { x: endX, y: endY } = end || {}
+			const chunk = start ? _chunk.lo(startX, startY).hi(endX - startX, endY - startY) : undefined
+			this.push({ skip: !start, x: startX, y: startY, chunk }) // if start is undefined that mean there is no change and the map should be sent with column 0
 		}
+		this.last = _chunk
 		next()
 	}
-}
-
-export class Framerate extends Transform {
-	constructor(fps) {
-		super()
-		this.limit = 1000 / fps
-		this.last = Date.now()
-	}
-
-	_transform(chunk, _, next) {
-		if (Date.now() - this.last > this.limit) {
-			this.push(chunk)
-			this.last = Date.now()
-		}
-		next()
-	}
-}
-
-export async function* nextFrame(stream) {
-	for await (let chunk of stream) yield chunk
 }
