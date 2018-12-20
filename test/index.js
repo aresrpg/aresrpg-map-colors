a9982db1ff442ee0f233ab535ca12b26d4d17bf9
Sceat <11330271+Sceat@users.noreply.github.com>
2018-12-20 03:42:26 +0200

    fix: text imports

--- test/index.js
+++ test/index.js
@@ -1,10 +1,10 @@
-import Colors, { COLORS } from '../src/mapColors'
+import { nearestMatch } from '../src/mapColors'
 import tape from 'blue-tape'
 import { ComputeColors, ComputeKnown, Framerate } from '../src/minecraftTransform'
 import { Readable, PassThrough } from 'stream'
 
 tape('Nearest match should return the closest minecraft compatible color', async t => {
-	return t.equal(Colors.nearestMatch(89, 125, 39), 4, `${Colors.nearestMatch(89, 125, 39)} should be 4`)
+	return t.equal(nearestMatch(89, 125, 39), 4, `${nearestMatch(89, 125, 39)} should be 4`)
 })
 
 tape('Framerate should throttle the stream', t => {
