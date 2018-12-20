9c02eb157369d2ced01732e43cee851d7137bbe0
Sceat <11330271+Sceat@users.noreply.github.com>
2018-12-20 10:45:10 +0200

    unit test of streams

--- test/index.js
+++ test/index.js
@@ -1,28 +1,58 @@
 import { nearestMatch } from '../src/mapColors'
 import tape from 'blue-tape'
-import { ComputeColors, ComputeKnown, Framerate } from '../src/minecraftTransform'
-import { Readable, PassThrough } from 'stream'
+import { ComputeColors, ComputeKnown } from '../src/minecraftTransform'
+import { Readable, Writable } from 'stream'
+import Deferred from '../src/deferredPromise'
 
 tape('Nearest match should return the closest minecraft compatible color', async t => {
 	return t.equal(nearestMatch(89, 125, 39), 4, `${nearestMatch(89, 125, 39)} should be 4`)
 })
 
-tape('Framerate should throttle the stream', t => {
-	t.plan(1)
-	const r = new Readable()
-	r._read = () => {}
-	const pss = new PassThrough()
-	r.pipe(new Framerate(10)).pipe(pss)
+tape('Next frame should be skipped if the last was exactly the same', async t => {
+	const def = new Deferred()
+	const image = 'https://i.imgur.com/nAcX5cX.png'
+	const amount = 5
+
+	const r = new ReadImage(image, amount)
 	let count = 0
-	let pushs = 0
-	pss.on('data', () => count++)
-	pss.on('finish', () => t.equal(count, 10, `${pushs} were pushed, we should have received 10 and received ${count}`))
-	const handle = setInterval(() => {
-		pushs++
-		r.push('slt')
-		if (pushs >= 100) {
-			r.push(null)
-			clearInterval(handle)
-		}
-	}, 10)
+
+	r.pipe(new ComputeColors())
+		.pipe(new ComputeKnown())
+		.pipe(
+			new WriteTest(c => {
+				const { skip, chunk } = c
+				if (!count) t.ok(chunk, 'The first frame should be there')
+				else t.ok(skip, 'any other frames should be skipped')
+				count++
+				if (count >= amount) def.resolve()
+			}),
+		)
+	return await def.promise
 })
+
+class ReadImage extends Readable {
+	constructor(image, amount) {
+		super()
+		this.image = image
+		this.amount = amount
+		this.count = 0
+	}
+
+	_read() {
+		if (this.count < this.amount) this.push(this.image)
+		else this.push(null)
+		this.count++
+	}
+}
+
+class WriteTest extends Writable {
+	constructor(callback) {
+		super({ objectMode: true })
+		this.callback = callback
+	}
+
+	_write(chunk, _, done) {
+		this.callback(chunk)
+		done()
+	}
+}
